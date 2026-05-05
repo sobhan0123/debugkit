f6d89e6a08cc1a22b00cebb681b96374a937b82e
Sobhan <s66056299@gmail.com>
2026-05-05 12:50:33 +0330

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="debugkit",
+    version="0.1.0",
+    description="Simple debugging toolkit for Python",
+    author="Your Name",
+    packages=find_packages(),
+    python_requires=">=3.8",
+)
