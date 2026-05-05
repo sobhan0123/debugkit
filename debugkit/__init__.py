026d7cc41d2174c8b97c818d84129d85f99a658c
Sobhan <s66056299@gmail.com>
2026-05-05 12:55:41 +0330

    Create __init__.py

---
+++ debugkit/__init__.py
@@ -0,0 +1,6 @@
+from .core.debug import debug
+from .core.trace import trace
+from .core.timer import timer
+from .version import version as version
+
+all = ["debug", "trace", "timer", "__version__"]
